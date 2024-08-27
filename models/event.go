e4e0ecafd3570ef707bc4e84a02fd3e615ff64a2
ngirimana <chadrackngirimana@gmail.com>
2024-08-27 07:58:11 +0200

    delete event

--- models/event.go
+++ models/event.go
@@ -88,3 +88,16 @@ func (e Event) Update() error {
 
 	return err
 }
+
+func (e Event) Delete() error {
+	query := `DELETE FROM events WHERE id = ?`
+
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(e.ID)
+
+	return err
+}
--- routes/events.go
+++ routes/events.go
@@ -84,3 +84,23 @@ func updateEvent(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "Event updated!", "event": updatedEvent})
 
 }
+
+func deleteEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID."})
+		return
+	}
+	event, error := models.GetEvent(id)
+	if error != nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": "Event not found."})
+		return
+	}
+
+	err = event.Delete()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete event."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted!"})
+}
